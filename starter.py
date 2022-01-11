292d48e23910759c93e4da04655f8225dac6395b
pvthinker <roullet@univ-brest.fr>
2022-01-11 08:57:02 +0100

    Change nx in starter

--- starter.py
+++ starter.py
@@ -2,8 +2,8 @@ import numpy as np
 import namelist
 import wave2d
 
-import matplotlib.pyplot as plt
-plt.ion()
+#import matplotlib.pyplot as plt
+#plt.ion()
 
 # to get the list of all parameters and their available values
 # use param.manall() in your IPython console or in your Jupyter notebook
@@ -19,7 +19,7 @@ param.aspect_ratio = 1.
 
 param.varplot = 'p'
 
-param.nx, param.ny = 128*4, 128*4
+param.nx, param.ny = 128*4, 128*2
 param.Lx, param.Ly = 4, 2
 param.x0, param.y0 = 2., 1.
 
@@ -36,11 +36,12 @@ param.tplot = .04  # smaller 'tplot' makes the animation smoother
 param.plotvector = 'None'  # 'velocity'
 param.vectorscale = 20.  # larger 'vectorscale' makes the arrows shorter
 param.dt = 0.02
+param.macuser = False # <- try True if the animation does not work
 
 param.U = .2
 param.alphaU = 0*np.pi/180.
 
-param.cax = np.asarray([-1., 1.])*2
+param.cax = np.asarray([-1., 1.])*.25
 param.figwidth = 1080
 
 model = wave2d.Wave2d(param)
