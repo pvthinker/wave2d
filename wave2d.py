Fix a flip sign in the wake, now we have high pressure afront
Thanks to an anymous Student at FDSE 2022

wave2d.py
@@ -167,7 +167,7 @@ class Wave2d(object):
                     if kt == 0:
                         kalpha = np.cos(param.alphaU)*kxx + \
                             np.sin(param.alphaU)*kyy
-                    hphi += (1j*1e2*dt*self.boat*param.U*kalpha) * \
+                    hphi -= (1j*1e2*dt*self.boat*param.U*kalpha) * \
                         np.exp(-1j*(kxx*xb+kyy*yb))
                     xb += dt*param.U*np.cos(param.alphaU)
                     yb += dt*param.U*np.sin(param.alphaU)